0855f09294de5babd266c01cd36b05989161740c
Andreas Stange <anst.dev@gmail.com>
2020-11-18 18:15:50 +0100

    added assets (editor scripts with tests, demo scene, and the main script files)

---
+++ UniInject/Assets/Scenes/DependencyInjectionDemo/LazyInjectionDemo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+// An instance of this class is injected only when needed, because the Binder is binding the instance lazy.
+public class LazyInjectionDemo
+{
+    private static LazyInjectionDemo instance;
+    public static LazyInjectionDemo Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new LazyInjectionDemo();
+                Debug.Log("Created instance of LazyInjectionDemo");
+            }
+            return instance;
+        }
+    }
+}
